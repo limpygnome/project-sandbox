02517f96586407b509a4d6e0c7588b01f93c24b3
limpygnome <limpygnome@gmail.com>
2015-05-08 15:58:41 +0100

    Save: frustrum culling next.

---
+++ website/src/main/webapp/resources/content/js/engine/frustrum.js
@@ -0,0 +1,27 @@
+projectSandbox.frustrum =
+{
+	// Far top left
+	ftlX: 0,
+	ftlY: 0,
+
+	// Far top right
+	ftrX: 0,
+	ftrY: 0,
+
+	// Far bottom left
+
+	// Far bottom right
+
+
+	// Near top left
+
+	// Near top right
+
+	// Near bottom left
+
+	// Near bottom right
+
+	update: function()
+	{
+	}
+}
