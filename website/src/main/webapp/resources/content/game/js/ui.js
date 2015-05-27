f985c7bcaeab4e2d0e794665d8abc1c8911a2c6d
limpygnome <limpygnome@gmail.com>
2015-05-27 22:14:17 +0100

    Feature: UI cleaned up. Need to add ammo/mags, or some update text, for items. Need to start score system and consider sound.

--- website/src/main/webapp/resources/content/game/js/ui.js
+++ website/src/main/webapp/resources/content/game/js/ui.js
@@ -1,26 +1,5 @@
 game.ui =
 {
-	// Size of the UI viewport
-	// TODO: update when resized
-	uiWidth: null,
-	uiHeight: null,
-	
-	// Indicates if to render UI regarding player
-	renderPlayerUI: false,
-	renderWeapon: false,
-	
-	// Weapon icon
-	iconWeapon: null,
-	
-	// Bounty / wanted stars
-	starsIcon: null,
-	starsOn: true,
-	
-	// Health
-	healthBar: null,
-
-
-
     // Elements - main
     elementRender: null,
     elementDeathScreen: null,
@@ -44,7 +23,6 @@ game.ui =
         this.elementUIInventory = document.getElementById("ps-ui-inventory");
 
 
-
 	    // Bind resize event for window
         $(window).resize(function () {
             game.ui.resize();
@@ -62,7 +40,6 @@ game.ui =
         $(".options.button.fullscreen").click(function () {
             game.ui.toggleFullScreen();
         });
-
 
 
 	    // Set UI size
@@ -104,42 +81,6 @@ game.ui =
         this.uiHeight = gl.viewportHeight;
 
         console.debug("engine/ui - size set to viewport - " + this.uiWidth + "x" + this.uiHeight);
-
-        // Rebuild UI
-        this.rebuildUI();
-	},
-	
-	rebuildUI: function()
-	{
-		var ratio = this.uiWidth / this.uiHeight;
-		
-		// Create weapons icon
-		this.iconWeapon = new Primitive(48 * ratio, 48 * ratio);
-		this.iconWeapon.setTexture("error");
-		this.setPrimitivePosTopLeft(this.iconWeapon, 8, 8);
-		
-		// Create health bar
-		this.healthBar = new PrimitiveBar(128, 8, true);
-		this.setPrimitivePosTopLeft(this.healthBar, 12, 78);
-		this.healthBar.setValue(0.5);
-		this.healthBar.setColour(
-			0.0,	1.0,	0.0,	1.0,
-			1.0,	0.0,	0.0,	1.0
-		);
-		
-		// Create bounty/wanted stars
-		var starCount = 6;
-		
-		this.starsIcon = [];
-		var star;
-		for (var i = 0; i < starCount; i++)
-		{
-			star = new Primitive(16 * ratio, 16 * ratio);
-			star.setTexture("ui/star_off");
-			this.setPrimitivePosTopLeft(star, 8 + ((star.width) * i), 90);
-			
-			this.starsIcon[i] = star;
-		}
 	},
 	
 	setPrimitivePosTopLeft: function(primitive, x, y)
@@ -155,6 +96,8 @@ game.ui =
 	
 	logic: function()
 	{
+	    // TODO: move this to be entirely event driven, so no logic loop is needed for UI
+
 		var plyEntId = projectSandbox.playerEntityId;
 		var ent = projectSandbox.entities.get(plyEntId);
 		
@@ -189,21 +132,17 @@ game.ui =
 			    this.renderPlayerUI = true;
 			}
 		}
-		else
-		{
-			this.renderPlayerUI = false;
-
-			if (!projectSandbox.network.closed)
-			{
-				console.warn("UI - unable to find player entity, cannot update UI");
-			}
-		}
+		else if (!projectSandbox.network.closed)
+        {
+            console.warn("UI - unable to find player entity, cannot update UI");
+        }
 	},
 	
 	render: function(gl, shaderProgram, modelView, perspective)
 	{
-		return;
+	    // Not used at present, replaced by HTML...
 
+	    /*
 	    // Disable depth for transparency
 	    gl.disable(gl.DEPTH_TEST);
 
@@ -211,42 +150,11 @@ game.ui =
 		mat4.ortho(perspective, 0, this.uiWidth, 0, this.uiHeight, 0, 1);
 		mat4.identity(modelView);
 
-		if (this.renderPlayerUI)
-		{
-			// Render cash score
-			
-			// Render health bar
-			this.healthBar.render(gl, shaderProgram, modelView, perspective);
-			
-			// Render weapon icon
-			if (this.renderWeapon)
-			{
-			    this.iconWeapon.render(gl, shaderProgram, modelView, perspective);
-			}
-			
-			// Render stars / bounty level
-			if (this.starsOn)
-			{
-				for(var i = 0; i < this.starsIcon.length; i++)
-				{
-					this.starsIcon[i].render(gl, shaderProgram, modelView, perspective);
-				}
-			}
-		}
+        // Do rendering here...
 
         // Re-enable depth testing
         gl.enable(gl.DEPTH_TEST);
-	},
-
-	hookPlayer_entChanged: function()
-	{
-	    // Assume inventory reset
-	    this.renderWeapon = false;
-	},
-
-	hookPlayer_entKilled: function(causeText)
-	{
-	    this.deathScreenShow(causeText);
+        */
 	},
 
     deathScreenShow: function(causeText)
@@ -337,6 +245,18 @@ game.ui =
             }
         }
 	},
+
+	hookPlayer_entChanged: function()
+    {
+        // Reset inventory
+        this.hook_inventoryReset();
+    },
+
+    hookPlayer_entKilled: function(causeText)
+    {
+        // Show death screen
+        this.deathScreenShow(causeText);
+    },
 
 	hook_inventorySlotCreate: function(inventoryItem)
 	{
