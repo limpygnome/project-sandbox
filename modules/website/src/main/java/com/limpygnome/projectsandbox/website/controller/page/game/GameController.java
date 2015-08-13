a1e1d7a5fa9b2a06528e23d4a261b9a37027898b
limpygnome <limpygnome@gmail.com>
2015-08-13 07:42:39 +0100

    Feature: game client can now be refreshed.

--- modules/website/src/main/java/com/limpygnome/projectsandbox/website/controller/page/game/GameController.java
+++ modules/website/src/main/java/com/limpygnome/projectsandbox/website/controller/page/game/GameController.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpSession;
+
 /**
  * Created by limpygnome on 19/04/2015.
  */
@@ -21,29 +23,47 @@ public class GameController extends BaseController
 
     private final static Logger LOG = LogManager.getLogger(GameController.class);
 
+    private static final String GAME_SESSION_TOKEN_ATTRIB = "game_session";
+
     @Autowired
     private GameSessionService gameSessionService;
 
     @RequestMapping(value = {"/game"})
-    public ModelAndView home(@ModelAttribute(MODEL_ATTRIB_GAME_SESSION_TOKEN) String gameSessionToken)
+    public ModelAndView home(@ModelAttribute(MODEL_ATTRIB_GAME_SESSION_TOKEN) String gameSessionToken,
+                             HttpSession httpSession)
     {
-        GameProvider gameProvider = new GameProvider();
-
-        // Check token is valid, else redirect back to home
-        if (!gameSessionService.validateExists(gameProvider, gameSessionToken))
+        // Pull token from session if not present
+        if (gameSessionToken == null || gameSessionToken.length() == 0)
         {
-            LOG.debug("Failed to validate and consume game session token");
-            return new ModelAndView("redirect:/home");
+            gameSessionToken = (String) httpSession.getAttribute(GAME_SESSION_TOKEN_ATTRIB);
         }
 
-        // Setup new page
-        ModelAndView modelAndView = createMV("game/main", null);
+        // Check token is valid, else redirect back to home
+        GameProvider gameProvider = new GameProvider();
 
-        modelAndView.addObject("game_session_token", gameSessionToken);
+        try
+        {
 
-        gameProvider.close();
+            if (!gameSessionService.validateExists(gameProvider, gameSessionToken))
+            {
+                LOG.debug("Failed to validate and consume game session token - token: {}", gameSessionToken);
+                return new ModelAndView("redirect:/home");
+            }
 
-        return modelAndView;
+            // Setup new page
+            ModelAndView modelAndView = createMV("game/main", null);
+
+            modelAndView.addObject("game_session_token", gameSessionToken);
+
+            // Store token into session to allow refresh of page
+            httpSession.setAttribute(GAME_SESSION_TOKEN_ATTRIB, gameSessionToken);
+
+            return modelAndView;
+        }
+        finally
+        {
+            gameProvider.close();
+        }
     }
 
 }
