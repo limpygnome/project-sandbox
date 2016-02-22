63aeb57c9d1b555644795ab120acb905476cbae9
limpygnome <limpygnome@gmail.com>
2016-02-22 07:39:00 +0000

    Progress with new and improved texture parsing. This will hopefully make parsing a lot easier, by specifying x/y/width/height, rather than each vertex individually.

--- modules/website/src/main/webapp/resources/content/engine/js/textures.js
+++ modules/website/src/main/webapp/resources/content/engine/js/textures.js
@@ -1,79 +1,82 @@
 projectSandbox.textures =
 {
-	textures: new Map(),
-	src: new Map(),
-	emptyTexture: null,
-	
-	flagColourDisabled: false,
-	
-	setup: function(gl)
-	{
-		var gl = projectSandbox.gl;
-		
-		// Fetch empty texture
-		this.emptyTexture = this.textures.get("white");
-	},
-	
-	get: function(name)
-	{
-		return this.textures.get(name);
-	},
-	
-	getSrc: function(name)
-	{
-		return this.src.get(name);
-	},
-	
-	logic: function()
-	{
-		// Execute logic for each texture
-		var texture;
-		for(var kv of this.textures)
-		{
-			texture = kv[1];
-			texture.logic();
-		}
-	},
-	
-	reset: function()
-	{
-		this.textures.clear();
-		this.src.clear();
-	},
-	
-	loadTextureJson: function(json)
-	{
-		var self = projectSandbox.textures;
-	
-		// Read src values
-		var name = json["name"];
-		var url = json["url"]
-		var width = json["width"];
-		var height = json["height"];
-		
-		// Create src entry
-		var src = new TextureSrc(name, url, width, height);
-		self.src.set(name, src);
-		console.log("Textures - added texture src '" + name + "'");
-		
-		// Parse textures
-		var textures = json["textures"];
-		var textureData;
-		var texture;
-		
-		for (var i = 0; i < textures.length; i++)
-		{
-			// Fetch JSON element
-			textureData = textures[i];
-			
-			// Create texture
-			texture = self.buildTexture(src, textureData);
-			
-			// Add to collection
-			self.textures.set(texture.name, texture);
-			console.log("Textures - added texture '" + texture.name + "' - " + texture.frames + " frames, " + texture.frameVertices + " vertices");
-		}
-	},
+    textures: new Map(),
+    src: new Map(),
+    emptyTexture: null,
+    
+    flagColourDisabled: false,
+    
+    setup: function(gl)
+    {
+        var gl = projectSandbox.gl;
+        
+        // Fetch empty texture
+        this.emptyTexture = this.textures.get("white");
+    },
+    
+    get: function(name)
+    {
+        return this.textures.get(name);
+    },
+    
+    getSrc: function(name)
+    {
+        return this.src.get(name);
+    },
+    
+    logic: function()
+    {
+        // Execute logic for each texture
+        var texture;
+        for(var kv of this.textures)
+        {
+            texture = kv[1];
+            texture.logic();
+        }
+    },
+    
+    reset: function()
+    {
+        this.textures.clear();
+        this.src.clear();
+    },
+    
+    loadTextureJson: function(json)
+    {
+        var self = projectSandbox.textures;
+    
+        // Read src values
+        var name = json["name"];
+        var url = json["url"]
+        var width = json["width"];
+        var height = json["height"];
+        
+        // Create src entry
+        var src = new TextureSrc(name, url, width, height);
+        self.src.set(name, src);
+        console.log("Textures - added texture src '" + name + "'");
+        
+        // Parse textures
+        var textures = json["textures"];
+        var textureData;
+        var texture;
+        
+        for (var i = 0; i < textures.length; i++)
+        {
+            // Fetch JSON element
+            textureData = textures[i];
+            
+            // Create texture
+            texture = self.buildTexture(src, textureData);
+            
+            // Add to collection
+            if (texture != null)
+            {
+                self.textures.set(texture.name, texture);
+                console.log("Textures - added texture '" + texture.name + "' - " + texture.frames + " frames, " + texture.frameVertices + " vertices");
+            }
+        }
+    },
     
     isLoaded : function()
     {
@@ -90,58 +93,96 @@ projectSandbox.textures =
         
         return true;
     },
-	
-	buildTexture : function(src, textureData)
-	{
-		// Load properties from texture
-		var name = textureData["name"];
-		var speed = textureData["speed"];
-		var frames = textureData["frames"];
-		
-		// Build framedata - convert each frame into array of vertices
-		// -- Check we have at least one frame
-		if (frames.length == 0)
-		{
-			console.error("Textures - cannot build texture - no frame data available - name: " + name);
-			return;
-		}
-		
-		// -- Check number of vertices per frame from first item
-		// -- -- Note: this is actually not verts, but total co-ordinates - divide by 2 for total verts
-		var expectedVertsPerFrame = frames[0].length;
-		if (expectedVertsPerFrame == 0)
-		{
-			console.error("Textures - cannot build texture - no vertex data - name: " + name);
-			return;
-		}
-		else if (expectedVertsPerFrame % 8 != 0)
-		{
-			console.error("Textures - cannot build texture - must be multiples of 8 vertices - name: " + name);
-			return;
-		}
-		
-		// -- frames * 8 floats (4 verts)
-		var frameData = new Float32Array(frames.length * expectedVertsPerFrame);
-		var frameDataOffset = 0;
-		var frame;
-		var frameVertices;
-		var vert;
+    
+    buildTexture : function(src, textureData)
+    {
+        var self = projectSandbox.textures;
+
+        // Load properties from texture
+        var name = textureData["name"];
+        var speed = textureData["speed"];
+        var type = textureData["type"];
+        var frames = textureData["frames"];
         
-		for (var f = 0; f < frames.length; f++)
-		{
-			frame = frames[f];
-			frameVertices = frame.length;
-			
-			// Check frame has correct number of vertices
-			if (frameVertices != expectedVertsPerFrame)
-			{
-				console.error("Textures - cannot build texture - differing number of vertices for frame " + f + " - name: " + name);
-				return;
-			}
-			
-			for (var v = 0; v < frameVertices; v++)
-			{
-                // Compue co-ordinate into unit vector
+        // Build framedata - convert each frame into array of vertices
+        // -- Check we have at least one frame
+        if (frames.length == 0)
+        {
+            console.error("Textures - cannot build texture - no frame data available - name: " + name);
+            return;
+        }
+
+        // Parse based on type
+        var framesResult;
+
+        switch (type)
+        {
+            case "generic":
+                framesResult = self.parseFramesTypeGeneric(frames, src);
+                break;
+            case "2d":
+                framesResult = self.parseFrameType2D(frames, src);
+                break;
+            case "3d":
+                //framesResult = self.parseFrameType3D(frames, src);
+                //break;
+                console.error("engine / textures - 3d not implemented");
+                return;
+            default:
+                console.error("engine / textures - cannot build texture - no type specified");
+                return;
+        }
+    
+        // Create texture from element
+        return new Texture(
+            src,
+            name,
+            speed,
+            frames.length,
+            framesResult.vertices,
+            framesResult.frameData
+        );
+    },
+
+    parseFramesTypeGeneric: function(frames, src)
+    {
+        // -- Check number of vertices per frame from first item
+        // -- -- Note: this is actually not verts, but total co-ordinates - divide by 2 for total verts
+        var expectedVertsPerFrame = frames[0].length;
+        if (expectedVertsPerFrame == 0)
+        {
+            console.error("Textures - cannot build texture - no vertex data - name: " + name);
+            return;
+        }
+        // TODO: consider if this check is actually needed anymore, can probably drop it...
+        else if (expectedVertsPerFrame % 8 != 0)
+        {
+            console.error("Textures - cannot build texture - must be multiples of 8 vertices - name: " + name);
+            return;
+        }
+
+        // -- frames * 8 floats (4 verts)
+        var frameData = new Float32Array(frames.length * expectedVertsPerFrame);
+        var frameDataOffset = 0;
+        var frame;
+        var frameVertices;
+        var vert;
+
+        for (var f = 0; f < frames.length; f++)
+        {
+            frame = frames[f];
+            frameVertices = frame.length;
+
+            // Check frame has correct number of vertices
+            if (frameVertices != expectedVertsPerFrame)
+            {
+                console.error("Textures - cannot build texture - differing number of vertices for frame " + f + " - name: " + name);
+                return;
+            }
+
+            for (var v = 0; v < frameVertices; v++)
+            {
+                // Compute co-ordinate into unit vector
                 if (v % 2 == 0)
                 {
                     vert = frame[v] / src.width;
@@ -152,22 +193,85 @@ projectSandbox.textures =
                     // from 1 to invert y axis
                     vert = frame[v] / src.height;
                 }
-                
+
                 // Set vertex co-ordinate
-				frameData[frameDataOffset++] = vert;
-			}
-		}
-	
-		// Create texture from element
-		return new Texture(
-			src,
-			name,
-			speed,
-			frames.length,
-			expectedVertsPerFrame / 2.0,
-			frameData
-		);
-	},
+                frameData[frameDataOffset++] = vert;
+            }
+        }
+
+        return {
+                    "vertices" : expectedVertsPerFrame / 2,
+                    "frameData" : frameData
+               };
+    },
+
+    parseFrameType2D : function(frames, src)
+    {
+        var self = projectSandbox.textures;
+
+        var coordinatesPerFrame = 8; // 8 per face / 4 pairs of XY
+        var frameData = new Float32Array(frames.length * coordinatesPerFrame);
+
+        for (var f = 0; f < frames.length; f++)
+        {
+            frame = frames[f];
+
+            // Compute frame data
+            self.buildFrameDataFourVerts(
+                frameData, f * coordinatesPerFrame,
+                src,
+                frame["x"], frame["y"], frame["width"], frame["height"]
+            );
+        }
+
+        return {
+                    "vertices" : coordinatesPerFrame / 2,
+                    "frameData" : frameData
+               };
+    },
+
+    parseFrameType3D : function(frames, src)
+    {
+        var coordinatesPerFrame = 8 * 6; // 8 per face, 6 faces
+        var frameData = new Float32Array(frames.length * coordinatesPerFrame);
+
+        return {
+                    "vertices" : coordinatesPerFrame / 2,
+                    "frameData" : frameData
+               };
+    },
+
+    buildFrameDataFourVerts: function(frameDataArray, frameDataOffset, src, x, y, width, height)
+    {
+        // -- Bottom,left
+        frameDataArray[frameDataOffset + 0] = x;
+        frameDataArray[frameDataOffset + 1] = y;
+
+        // -- Top,left
+        frameDataArray[frameDataOffset + 2] = x;
+        frameDataArray[frameDataOffset + 3] = y + (height - 1);
+
+        // -- Top,right
+        frameDataArray[frameDataOffset + 4] = x + (height - 1);
+        frameDataArray[frameDataOffset + 5] = y + (height - 1);
+
+        // -- Top,bottom
+        frameDataArray[frameDataOffset + 4] = x + (height - 1);
+        frameDataArray[frameDataOffset + 5] = y;
+
+        // Now convert co-ordinates into unit vector
+        for (var i = 0; i < 8; i++)
+        {
+            if (i % 2 == 0)
+            {
+                frameDataArray[frameDataOffset + i] / src.width;
+            }
+            else
+            {
+                frameDataArray[frameDataOffset + i] / src.height;
+            }
+        }
+    },
     
     switchFrameY: function(frameData, index1, index2)
     {
@@ -175,14 +279,14 @@ projectSandbox.textures =
         frameData[index1] = frameData[index2];
         frameData[index2] = v;
     },
-	
-	bindNoTexture: function(gl, shaderProgram)
-	{
-		this.emptyTexture.bind(gl, shaderProgram);
-	},
-	
-	unbindNoTexture: function(gl, shaderProgram)
-	{
-		this.emptyTexture.unbind(gl, shaderProgram);
-	}
+    
+    bindNoTexture: function(gl, shaderProgram)
+    {
+        this.emptyTexture.bind(gl, shaderProgram);
+    },
+    
+    unbindNoTexture: function(gl, shaderProgram)
+    {
+        this.emptyTexture.unbind(gl, shaderProgram);
+    }
 }
--- modules/website/src/main/webapp/resources/content/engine/js/texturesrc.js
+++ modules/website/src/main/webapp/resources/content/engine/js/texturesrc.js
@@ -1,15 +1,15 @@
 function TextureSrc(name, url, width, height, texture)
 {
-	var gl = projectSandbox.gl;
+    var gl = projectSandbox.gl;
 
-	// Assign data
-	this.name = name;
+    // Assign data
+    this.name = name;
     this.width = width;
     this.height = height;
 
-	if (url != null)
-	{
-	    this.url = url;
+    if (url != null)
+    {
+        this.url = url;
 
         // Create new texture
         this.texture = gl.createTexture();
@@ -26,32 +26,32 @@ function TextureSrc(name, url, width, height, texture)
             console.error("Texture src - failed to load texture - " + url);
         };
         this.image.src = url;
-	}
-	else if (texture != null)
-	{
-	    this.texture = texture;
-	}
+    }
+    else if (texture != null)
+    {
+        this.texture = texture;
+    }
 }
 
 TextureSrc.prototype.bindData = function(gl)
 {
-	// Bind to texture
-	gl.bindTexture(gl.TEXTURE_2D, this.texture);
-	
-	// Set the texture data
-	gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, this.image);
-	
-	// Set texture properties - we allow non-power of two images
-	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
-	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
-	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+    // Bind to texture
+    gl.bindTexture(gl.TEXTURE_2D, this.texture);
+    
+    // Set the texture data
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, this.image);
+    
+    // Set texture properties - we allow non-power of two images
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
 
-	// Generate mipmap for scaling
-	gl.generateMipmap(gl.TEXTURE_2D);
-	
-	// Unbind texture
-	gl.bindTexture(gl.TEXTURE_2D, null);
-	
-	console.log("Texture src - loaded texture file - '" + this.image.src + "'");
+    // Generate mipmap for scaling
+    gl.generateMipmap(gl.TEXTURE_2D);
+    
+    // Unbind texture
+    gl.bindTexture(gl.TEXTURE_2D, null);
+    
+    console.log("Texture src - loaded texture file - '" + this.image.src + "'");
 }
